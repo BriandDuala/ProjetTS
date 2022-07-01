dc31e1b518f85914bd1b8533abe9e386351e3596
BriandDuala <68736784+BriandDuala@users.noreply.github.com>
2022-07-01 18:01:17 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,9 @@
-
 const mocha = require('gulp-mocha');
-
-
 gulp.task('./test-results.xml', function () {
     gulp.src('tests/**/*.js')
         .pipe(mocha({
           reporter: 'mocha-junit-reporter',
-          reporterOptions: {
+          reporterOptions:{
             mochaFile: './TEST-.xml'
           } 
         }))    
