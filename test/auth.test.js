e689cbc7e1413d3e67ae0b6ca340fc7849c592b2
BriandDuala <68736784+BriandDuala@users.noreply.github.com>
2022-07-06 08:40:31 +0100

    semi final

--- test/auth.test.js
+++ test/auth.test.js
@@ -18,10 +18,44 @@ describe("POST /users", () => {
 
   describe("Bad request", () => {
 
-    it("Poor or medium password", async () => {
+    it("Poor password", async () => {
       const bodyData = [
         {username: "username"},
         {password: "password"}
+      ]
+      for (const body of bodyData) {
+        const response = await request(app).post("/users").send(body)
+        assert.equal(response.statusCode, 400)
+      }
+    })
+
+    it("Medium password", async () => {
+      const bodyData = [
+        {username: "username"},
+        {password: "Pa55word"}
+      ]
+      for (const body of bodyData) {
+        const response = await request(app).post("/users").send(body)
+        assert.equal(response.statusCode, 400)
+      }
+    })
+
+    it("Password too short expect to be >= 8 char", async () => {
+      const bodyData = [
+        {username: "username"},
+        {password: "pass"}
+      ]
+      for (const body of bodyData) {
+        const response = await request(app).post("/users").send(body)
+        assert.equal(response.statusCode, 400)
+      }
+    })
+
+    it("Username or passwords is incorrect", async () => {
+      const bodyData = [
+        {username: "username"},
+        {password: "password"},
+        {}
       ]
       for (const body of bodyData) {
         const response = await request(app).post("/users").send(body)
